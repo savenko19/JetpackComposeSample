dd8076a83ce4cb8eedc15a4b36453ecd06e4bec6
Владимир Савенко <vladimir.savenko@myworkflow.ru>
2021-11-08 21:38:22 +0700

    definition composable preview function

--- app/src/main/java/com/example/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposesample/MainActivity.kt
@@ -3,23 +3,26 @@ package com.example.jetpackcomposesample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackcomposesample.ui.theme.JetpackComposeSampleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageText(message = "Hello Android!")
+            PreviewMessageText()
         }
     }
     
     @Composable
     fun MessageText(message: String) {
         Text(text = "Message text: $message")
+    }
+    
+    @Preview
+    @Composable
+    fun PreviewMessageText() {
+        MessageText(message = "Hello World")
     }
 }
