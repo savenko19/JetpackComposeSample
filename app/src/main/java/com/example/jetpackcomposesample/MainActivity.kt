531c012ab4d836250d09599d92d4ee165d0af560
Vladimir Savenko <savenko19@gmail.com>
2021-11-09 02:01:03 +0700

    Animated list item (#2)
    * adding compose ui tool dependencies
    
    * definition composable function
    
    * definition composable preview function
    
    * adding an object to display news
    
    * display title, description and news icon
    
    * adding Material Design components
    
    * adding list and scroll state
    
    * enabled dark theme
    
    * changing list
    
    * adding animation to list item click
    
    Co-authored-by: Владимир Савенко <vladimir.savenko@myworkflow.ru>

--- app/src/main/java/com/example/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposesample/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.example.jetpackcomposesample
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,6 +24,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposesample.data.News
+import com.example.jetpackcomposesample.data.news
 import com.example.jetpackcomposesample.ui.theme.JetpackComposeSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,24 +32,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeSampleTheme {
-                NewsList()
+                NewsList(news)
             }
         }
     }
 
 
     @Composable
-    fun NewsList() {
-        val scrollState = rememberLazyListState()
-        LazyColumn(state = scrollState) {
-            items(50) {
-                NewsCard(News(
-                    title = "Jetpack Compose",
-                    description = "It's composable function"
-                ))
+    fun NewsList(news: List<News>) {
+        LazyColumn {
+            items(news) { newsItem ->
+                NewsCard(newsItem)
             }
         }
     }
+
     @Composable
     fun NewsCard(news: News) {
         //Column fun lets you arrange elements vertically
@@ -69,26 +69,43 @@ class MainActivity : ComponentActivity() {
                 .fillMaxWidth()
         ) {
 
-            Column {
+            var isExpanded by remember { mutableStateOf(false) }
+            val surfaceColor: Color by animateColorAsState(
+                if (isExpanded) Color.DarkGray else Color.White
+            )
+            Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
                 Text(
                     text = news.title,
                     color = Color.Gray,
                     style = MaterialTheme.typography.h5
                 )
                 Spacer(modifier = Modifier.height(4.dp))
-                Text(
-                    text = news.description,
-                    color = Color.LightGray,
-                    style = MaterialTheme.typography.subtitle1
-                )
+                androidx.compose.material.Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    elevation = 1.dp,
+                    color = surfaceColor,
+                    modifier = Modifier
+                        .animateContentSize()
+                        .padding(1.dp)
+                ) {
+                    Text(
+                        text = news.description,
+                        modifier = Modifier.padding(all = 4.dp).width(64.dp),
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                        style = MaterialTheme.typography.body2
+                    )
+                }
             }
             Spacer(modifier = Modifier.width(8.dp))
+
+
+            val imageSize = if (isExpanded) 100.dp else 40.dp
 
             Image(
                 painter = painterResource(R.drawable.ic_launcher_background),
                 contentDescription = "News icon",
                 modifier = Modifier
-                    .size(40.dp)
+                    .size(imageSize)
                     .clip(RectangleShape)
                     .border(2.dp, Color.DarkGray, RectangleShape)
                     .align(Alignment.CenterEnd)
@@ -97,7 +114,12 @@ class MainActivity : ComponentActivity() {
 
     }
 
-    @Preview
+    @Preview(name = "Light Mode")
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
     @Composable
     fun PreviewNewsCard() {
         JetpackComposeSampleTheme {
